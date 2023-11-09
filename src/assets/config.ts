d9e8482b1b7bb548fffb812f1bd87fd0c17abdd8
Sean Smith <sean.smith@student.unsw.edu.au>
2023-11-09 13:40:32 +1100

    Switch COnfig

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,2 +1,2 @@
-// export const URL_CONFIG = 'https://congregate-backend.onrender.com/';
-export const URL_CONFIG = 'http://127.0.0.1:5000';
+export const URL_CONFIG = 'https://congregate-backend.onrender.com/';
+// export const URL_CONFIG = 'http://127.0.0.1:5000';
